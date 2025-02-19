269ecce72add8f6a6c8adc9e761e76b2193da3cf
Varenya Upadhyaya <varenyau@gmail.com>
2025-02-19 13:08:04 -0500

    create netrw config

--- lua/celadon/init.lua
+++ lua/celadon/init.lua
@@ -1,5 +1,6 @@
 require('celadon.remap')
 require('celadon.lazy_init')
 require('celadon.options')
+require('celadon.netrw')
 
 print('hello check3')
---
+++ lua/celadon/netrw.lua
@@ -0,0 +1,3 @@
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 1
+vim.g.netrw_altv = 1
