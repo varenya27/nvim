adad4f7bc8b045d74429bfd26e4cbaf0e678db9b
Varenya Upadhyaya <varenyau@gmail.com>
2026-02-24 23:32:45 -0500

    synctex thingz

--- lua/celadon/lazy/vimtex.lua
+++ lua/celadon/lazy/vimtex.lua
@@ -3,6 +3,8 @@ return {
 	lazy = false,
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_indent_on_enter = 1
+		vim.g.vimtex_indent_lists={'enumerate','itemize'}
 		vim.keymap.set("n","<leader><CR>",vim.cmd.VimtexCompile,{desc = "compile tex file"})
 	end
 }
